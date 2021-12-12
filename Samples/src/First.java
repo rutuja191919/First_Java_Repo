459ed6a224ec9b9879c02ec2b13dbb8fd7b29caa
Simplilearn Tutorial GitHub <rutuja.c@simplilearn.net>
2021-12-12 10:58:03 +0530

    This is the second commit with changes in the file First.java

---
+++ Samples/src/First.java
@@ -0,0 +1,10 @@
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello !! This is the java file for Jenkins demo");
+
+	}
+
+}
